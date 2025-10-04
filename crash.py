23db19fe76ea640e4cc496c920133317ad00cbe4
KunFay99 <kaffayukiffu@gmail.com>
2025-10-04 18:22:05 +0700

    Update crash.py

--- crash.py
+++ crash.py
@@ -32,8 +32,8 @@ print("""
 \033[94m╚════════════════════════════════════════════════════════════╝
 """)
 while attemps < 100:
-    username = input('\033[33m Enter your username: \033[32m')
-	password = input('\033[32m Enter your password: \033[32m')
+       username = input('\033[33m Enter your username: \033[32m')
+       password = input('\033[32m Enter your password: \033[32m')
     if username == 'kun' and password == 'fayakun':
         print("\033[32m-------$ELAMAT DATANG DI ZONA MOSLEM CYBER ARMY------")
         break
